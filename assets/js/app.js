335d4df3d4932896e71e3481ffcbaab615d0f4a6
Muhammet Mücahit Özekinci <m.mucahitozekinci@gmail.com>
2024-02-02 16:15:36 +0300

    Chat scroll
    When new message comes chat has to scroll bottom.

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,10 +22,26 @@ import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
+let Hooks = {}
+Hooks.ChatScroll = {
+  mounted() {
+    this.el.scrollTo(0, this.el.scrollHeight);
+  },
+  updated() {
+    const pixelsBelowBottom =
+      this.el.scrollHeight - this.el.clientHeight - this.el.scrollTop;
+
+    if (pixelsBelowBottom < this.el.clientHeight * 0.3) {
+      this.el.scrollTo(0, this.el.scrollHeight);
+    }
+  } 
+}
+
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket('/live', Socket, {
-        params: { _csrf_token: csrfToken }
-      });
+  hooks: Hooks, params: { _csrf_token: csrfToken }
+});
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
